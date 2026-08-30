54f88554b615753887d048d74e2c3a98bb4c4cf3
CyberGloom <infectiouskitty@gmail.com>
2026-08-30 09:23:12 -0500

    chore: auto-update flake inputs and system config

--- configuration.nix
+++ configuration.nix
@@ -81,7 +81,8 @@
     enable = true;
     # Allow connections from your home network
     # This automatically modifies config.yaml behind the scenes
-    listen = true; 
+    listen = true;
+    dataDIR = "/var/lib/sillytavern"; 
   };
 
   # Open port 8000 in the NixOS firewall so your phone can reach it
